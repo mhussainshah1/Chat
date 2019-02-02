3d224705ef0686387f970af6234b0aa6b8519b06
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-02-02 16:52:00 -0500

    Make MassageObject bean class

--- src/chat/client/MessageObject.java
+++ src/chat/client/MessageObject.java
@@ -2,9 +2,90 @@ package chat.client;
 //This class is updated
 
 public class MessageObject {
-
+    //TO-Do
+    //Make all varibles private and use bean methods in class
+    
     String message;//null
-    int startX, startY, width, height;//0
-    boolean isImage, selected, isIgnored; //false
+    int startX;
+    int startY;
+    int width;
+    int height;//0
+    boolean isImage;
+    boolean selected;
+    boolean isIgnored; //false
     int MessageType;//0
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStartX() {
+        return startX;
+    }
+
+    public void setStartX(int startX) {
+        this.startX = startX;
+    }
+
+    public int getStartY() {
+        return startY;
+    }
+
+    public void setStartY(int startY) {
+        this.startY = startY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public boolean isIsImage() {
+        return isImage;
+    }
+
+    public void setIsImage(boolean isImage) {
+        this.isImage = isImage;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public boolean isIsIgnored() {
+        return isIgnored;
+    }
+
+    public void setIsIgnored(boolean isIgnored) {
+        this.isIgnored = isIgnored;
+    }
+
+    public int getMessageType() {
+        return MessageType;
+    }
+
+    public void setMessageType(int MessageType) {
+        this.MessageType = MessageType;
+    }
+
+    
 }
